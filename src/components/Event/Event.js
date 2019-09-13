a9a4627a0e7d26b6e9bdfdd9288961907d9d851f
Aidan McKay <aidanmckay2000@gmail.com>
2019-09-13 13:48:17 -0600

    add description limiter, add click to view full description later

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -5,7 +5,8 @@ export class Event extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      formattedAddress: ''
+      formattedAddress: '',
+      trimDesc: ''
     }
   }
   componentDidMount = async () => {
@@ -13,21 +14,25 @@ export class Event extends Component {
     const geoResults = await getAddress(location[1], location[0])
     const formattedAddress = geoResults.find(address => address)
     this.setState({ formattedAddress })
+    this.trimDescription()
   }
   
   trimDescription = () => {
     const { description } = this.props
+    const words = description.split(' ');
+    const trimDesc = words.slice(0, 7).join(' ') + '...'
+    this.setState({ trimDesc })
   }
 
   render() {
-    const { title, description } = this.props 
-    const { formattedAddress } = this.state
+    const { title, description} = this.props 
+    const { formattedAddress, trimDesc } = this.state
     return (
       <section>
         <h4>{!title && 'No available title'}</h4>
         <h4>{title}</h4>
         <p>{!description && 'No available Description'}</p>
-        <p>{description}</p>
+        <p>{trimDesc}</p>
         <p>{!formattedAddress && 'No available Address'}</p>
         <p>{formattedAddress}</p>
       </section>
