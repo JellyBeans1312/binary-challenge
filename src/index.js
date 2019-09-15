d21bd086d5f0dca29c6be5e575917e6e109bf686
Aidan McKay <aidanmckay2000@gmail.com>
2019-09-15 13:28:44 -0600

    add react-router to application

--- src/index.js
+++ src/index.js
@@ -7,14 +7,17 @@ import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from './reducers';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
 
 
 const store = createStore(rootReducer, composeWithDevTools())
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
