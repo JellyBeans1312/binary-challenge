a3c7cfdc1453cf936d82352ab6281671382d8431
Aidan McKay <aidanmckay2000@gmail.com>
2019-09-15 21:00:38 -0600

    add navlinks to navbar component and render components

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { NavLink, Route } from 'react-router-dom'
+import  FilterEvents   from '../../containers/FilterEvents/FilterEvents'
+import  EventContainer  from '../EventContainer/EventContainer';
+import  SearchLocation  from '../../containers/SearchLocation/SearchLocation';
 
 export const Navbar = () => {
   return (
-    <nav>
-      <p>All Events</p>
-      <p>Search events by location</p>
-      <p>Filter</p>
-    </nav>
+    <>
+      <nav>
+      <NavLink exact to='/'>All Events</NavLink>
+      <NavLink exact to='/near-you'>Search events by location</NavLink>
+      <NavLink exact to='filter-events'>Filter</NavLink>
+      </nav>
+      <section>
+        <Route exact path='/' component={EventContainer}/>
+        <Route exact path='/near-you' component={SearchLocation}/>
+        <Route exact path='/filter-events' component={FilterEvents}/>
+      </section>
+    </>
+
   )
 }
+
