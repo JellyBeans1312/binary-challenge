24670fe4f2d258ce6bbb8c24215d6a3e456d9923
Aidan McKay <aidanmckay2000@gmail.com>
2019-09-16 14:56:41 -0600

    SearchLocation component tests are done, need to fix a couple bugs and add test for mDTP

--- src/containers/SearchLocation/SearchLocation.test.js
+++ src/containers/SearchLocation/SearchLocation.test.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { SearchLocation } from './SearchLocation';
-import getLocation   from '../../apiCalls/apiCalls';
+import { getLocation, getEventsByLocation }  from '../../apiCalls/apiCalls';
 
 jest.mock('../../apiCalls/apiCalls')
 
 describe('SearchLocation', () => {
   let wrapper;
-  let mockEvent
+  let mockEvent;
+
   const mockAddEvents = jest.fn()
+
+  const mockEvents = [
+    { title: "something", category: 'concert', location: [{lat: 23, long: 234}] }
+  ];
+
   beforeEach(() => {
     mockEvent = {preventDefault: jest.fn()}
     wrapper = shallow(
@@ -16,6 +22,9 @@ describe('SearchLocation', () => {
         addEvents={mockAddEvents}
       />
     )
+    getLocation.mockImplementation(() => {
+      return Promise.resolve([{address: 'something 123 something else drive'}])
+    });
   });
 
   it('should match snapshot', () => {
@@ -38,8 +47,22 @@ describe('SearchLocation', () => {
       throw new Error("Please Try Again");
     });
 
-    await wrapper.instance().searchEventByLocation(mockEvent);
+    wrapper.instance().searchEventByLocation(mockEvent);
 
     expect(wrapper.state("error")).toEqual("There was a problem getting your search results");
   });
+
+  it('should call getEventsByLocation when searchEventByLocation is invoked', () => {
+    getEventsByLocation.mockImplementation(() => {
+      return Promise.resolve(mockEvents);
+    });
+    wrapper.instance().searchEventByLocation(mockEvent)
+    expect(getEventsByLocation).toHaveBeenCalled()
+  });
+
+  it('should call add events when searchEventByLocation is invoked', () => {
+  
+    wrapper.instance().searchEventByLocation(mockEvent)
+    expect(mockAddEvents).toHaveBeenCalled()
+  })
 })
