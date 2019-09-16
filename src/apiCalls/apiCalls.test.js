1fd8c46e5ae258c8c81f26a3db52d40a6e197ba6
Aidan McKay <aidanmckay2000@gmail.com>
2019-09-16 17:58:22 -0600

    add happy and sad tests for getAddress call

--- src/apiCalls/apiCalls.test.js
+++ src/apiCalls/apiCalls.test.js
@@ -124,4 +124,114 @@ describe('apiCalls', () => {
       expect(searchEvent('jid')).rejects.toEqual(Error('Failed to find event, please try again later'))
     });
   });
+
+  describe('getAddress', () => {
+    let mockResponse;
+    beforeEach(() => {
+      mockResponse = {
+      "results" : [
+        {
+           "address_components" : [
+              {
+                 "long_name" : "1600",
+                 "short_name" : "1600",
+                 "types" : [ "street_number" ]
+              },
+              {
+                 "long_name" : "Amphitheatre Pkwy",
+                 "short_name" : "Amphitheatre Pkwy",
+                 "types" : [ "route" ]
+              },
+              {
+                 "long_name" : "Mountain View",
+                 "short_name" : "Mountain View",
+                 "types" : [ "locality", "political" ]
+              },
+              {
+                 "long_name" : "Santa Clara County",
+                 "short_name" : "Santa Clara County",
+                 "types" : [ "administrative_area_level_2", "political" ]
+              },
+              {
+                 "long_name" : "California",
+                 "short_name" : "CA",
+                 "types" : [ "administrative_area_level_1", "political" ]
+              },
+              {
+                 "long_name" : "United States",
+                 "short_name" : "US",
+                 "types" : [ "country", "political" ]
+              },
+              {
+                 "long_name" : "94043",
+                 "short_name" : "94043",
+                 "types" : [ "postal_code" ]
+              }
+           ],
+           "formatted_address" : "1600 Amphitheatre Parkway, Mountain View, CA 94043, USA",
+           "geometry" : {
+              "location" : {
+                 "lat" : 37.4224764,
+                 "lng" : -122.0842499
+              },
+              "location_type" : "ROOFTOP",
+              "viewport" : {
+                 "northeast" : {
+                    "lat" : 37.4238253802915,
+                    "lng" : -122.0829009197085
+                 },
+                 "southwest" : {
+                    "lat" : 37.4211274197085,
+                    "lng" : -122.0855988802915
+                 }
+              }
+           },
+           "place_id" : "ChIJ2eUgeAK6j4ARbn5u_wAGqWA",
+           "types" : [ "street_address" ]
+        }
+     ],
+     "status" : "OK"
+  }
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+      });
+    });
+  })
+
+    it('should be called with the correct params', () => {
+        getAddress('Mountain View', 'CA')
+        expect(window.fetch).toBeCalledWith('https://maps.googleapis.com/maps/api/geocode/json?latlng=Mountain View,CA&key=AIzaSyDPPlNyjATG8sQnfsdg45ln7kUoyFH24m0')
+    });
+
+    it('should return a successful response', () => {
+      expect(getAddress('Mountain View', 'CA')).resolves.toEqual(mockResponse);
+    });
+
+    it('should return an error', () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        });
+      });
+      expect(getAddress('Denver', 'CO')).rejects.toEqual(Error('Failed to find address'));
+    });
+
+    it('should return an error if the promise rejects', () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.reject(Error('Failed to find address'))
+      });
+  
+      expect(searchEvent('jid')).rejects.toEqual(Error('Failed to find address'))
+    });
+  })
+
+  describe('getLocation', () => {
+
+  });
+
+  describe('getEventsByLocation', () => {
+
+  });
 })
