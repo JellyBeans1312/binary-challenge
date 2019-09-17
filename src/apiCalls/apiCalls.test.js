8964935e2e4314c27c381eb550f184319b5d4a12
Aidan McKay <aidanmckay2000@gmail.com>
2019-09-16 18:14:16 -0600

    add happy and sad tests for getEventsByLocation call

--- src/apiCalls/apiCalls.test.js
+++ src/apiCalls/apiCalls.test.js
@@ -317,18 +317,76 @@ describe('apiCalls', () => {
           ok: false
         });
       });
-      expect(getLocation('Mountain View', 'CA')).rejects.toEqual(Error('Failed to find address'));
+      expect(getLocation('Mountain View', 'CA')).rejects.toEqual(Error('Failed to get coordinates'));
     });
 
     it('should return an error if the promise rejects', () => {
       window.fetch = jest.fn().mockImplementation(() => {
-        return Promise.reject(Error('Failed to find address'))
+        return Promise.reject(Error('Failed to get coordinates'))
       });
   
-      expect(getLocation('Mountain View', 'CA')).rejects.toEqual(Error('Failed to find address'))
+      expect(getLocation('Mountain View', 'CA')).rejects.toEqual(Error('Failed to get coordinates'))
     });
   })
   describe('getEventsByLocation', () => {
+    let mockResponse;
+    let options;
+    beforeEach(() => {
+      options = { 
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer qJhnqW5Rsc36m6cUYXZyCaV2BaFNB0QoGpr4fmk_',
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }
+      }
+      mockResponse = {
+        relevance:1,
+        id:"Hp7hhoCTmR8twF9sBp",
+        title:"WWE Raw",
+        description:"",
+        category:"sports",
+        rank:76,
+        local_rank:93,
+        duration:0,
+        start:"2019-10-14T23:30:00Z",
+        end:"2019-10-14T23:30:00Z",
+        updated:"2019-09-12T17:07:04Z",
+        first_seen:"2019-08-13T16:12:32Z",
+        timezone:"America/Denver",
+      };
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockResponse)
+        });
+      });
+    });
+  
+    it('should be called with correct params', () => {
+      getEventsByLocation(12345, -12345)
+      expect(window.fetch).toBeCalledWith('https://api.predicthq.com/v1/events/?within=20mi@12345,-12345/', options)
+    });
+  
+    it('should return a successful response', () => {
+      expect(getEventsByLocation(12345, -12345)).resolves.toEqual(mockResponse);
+    });
 
+    it('should return an error', () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        });
+      });
+      expect(getEventsByLocation(12345, -12345)).rejects.toEqual(Error('Failed to get event from coordinates'));
+    });
+
+    it('should return an error if the promise rejects', () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.reject(Error('Failed to get event from coordinates'))
+      });
+  
+      expect(getEventsByLocation(12345, -12345)).rejects.toEqual(Error('Failed to get event from coordinates'))
+    });
   });
 })
