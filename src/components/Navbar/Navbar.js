f4f1fc73fefc8f839e198ce767ec8a4bdc91dfed
Aidan McKay <aidanmckay2000@gmail.com>
2019-09-16 20:41:44 -0600

    fix an issue where all events did not reset event in store

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { NavLink, Route } from 'react-router-dom'
-import  FilterEvents   from '../../containers/FilterEvents/FilterEvents'
-import  EventContainer  from '../../containers/EventContainer/EventContainer';
-import  SearchLocation  from '../../containers/SearchLocation/SearchLocation';
+import FilterEvents from '../../containers/FilterEvents/FilterEvents'
+import EventContainer from '../../containers/EventContainer/EventContainer';
+import SearchLocation from '../../containers/SearchLocation/SearchLocation';
+import { getEvents } from '../../apiCalls/apiCalls';
 
-export const Navbar = () => {
-  return (
-    <>
-      <nav>
-      <NavLink exact to='/'>All Events</NavLink>
-      <NavLink exact to='/near-you'>Search events by location</NavLink>
-      <NavLink exact to='filter-events'>Filter</NavLink>
-      </nav>
-      <section>
-        <Route exact path='/' component={EventContainer}/>
-        <Route exact path='/near-you' component={SearchLocation}/>
-        <Route exact path='/filter-events' component={FilterEvents}/>
-      </section>
-    </>
-
-  )
+export const Navbar = ({ addEvents }) =>  {
+   const sendEvents = async () => {
+    const randomEvents = await getEvents()
+    addEvents(randomEvents)
+  }
+    return (
+      <>
+        <nav>
+        <NavLink exact to='/' onClick={sendEvents}>All Events</NavLink>
+        <NavLink exact to='/near-you'>Search events by location</NavLink>
+        <NavLink exact to='filter-events'>Filter</NavLink>
+        </nav>
+        <section>
+          <Route exact path='/' component={EventContainer}/>
+          <Route exact path='/near-you' component={SearchLocation}/>
+          <Route exact path='/filter-events' component={FilterEvents}/>
+        </section>
+      </>
+  
+    )
 }
 
